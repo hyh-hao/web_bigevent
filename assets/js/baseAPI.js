4b362c366cc2375868afc4e97688852f9cb4af61
hyh-俞 <2967158862@qq.com>
2021-07-07 16:21:26 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -5,5 +5,38 @@
 $.ajaxPrefilter(function(options) {
     // 在发起真正的Ajax请求之前，统一拼接请求的根路径
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url;
+    console.log(options.url);
+
+
+    // 统一为有权限的接口 设置headers请求头
+
+    // 查看字符串中是否有/my有权限的地址
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+
+            // 获取用户存储到本地数据库中的信息 如果没有就为空字符串
+            Authorization: localStorage.getItem('token') || '',
+
+        }
+    }
+
+
+    // 全局统一挂载complete 回调函数
+    options.complete = function(res) {
+
+        // console.log('执行conmlete');
+        console.log(res);
+        // 在complet回调函数中，可以使用res.responseJSON
+        // 拿到服务器响应回来的数据
+        if (res.responseJSON.status == 1 && res.responseJSON.message == "身份认证失败！") {
+            // 1、强制清空token
+            localStorage.removeItem('token');
+            // 2、强制跳转到登录页面
+            location.href = '../../login.html';
+        }
+
+    }
+
+
 
 })
